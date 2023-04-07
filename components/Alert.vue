b71f976118fa0add898edf4a046a0b38987e1baf
Felice Moretta <felice_moretta@hotmail.it>
2023-04-08 00:43:45 +0200

    aggiunto overlay al background del layout cover e aggiunto alert

---
+++ components/Alert.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+  import { withDefaults } from 'vue';
+
+  withDefaults(defineProps<{ type?: 'info' | 'warning' | 'error' }>(), { type: 'info' });
+</script>
+
+<template>
+  <div class="alert" :class="`alert--${type}`">
+    <span class="alert__icon">
+      <mdi-information-outline v-if="type === 'info'" />
+      <mdi-alert-outline v-if="type === 'warning'" />
+      <mdi-alert-circle-outline v-if="type === 'error'" />
+    </span>
+    <div class="alert__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .alert {
+    position: relative;
+    display: flex;
+    padding: 15px;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  .alert::before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    opacity: 0.15;
+    pointer-events: none;
+  }
+
+  .alert--info {
+    color: var(--color-info);
+  }
+
+  .alert--info::before {
+    background-color: var(--color-info);
+  }
+
+  .alert--warning {
+    color: var(--color-warning);
+  }
+
+  .alert--warning::before {
+    background-color: var(--color-warning);
+  }
+
+  .alert--error {
+    color: var(--color-error);
+  }
+
+  .alert--error::before {
+    background-color: var(--color-error);
+  }
+
+  .alert__icon {
+    display: flex;
+    align-items: center;
+    padding: 0 25px 0 10px;
+  }
+</style>
+
+<style>
+  .alert .alert__content > p {
+    margin: 0;
+  }
+</style>
--- theme/layouts/cover.vue
+++ theme/layouts/cover.vue
@@ -12,12 +12,22 @@ const props = defineProps({
 const style = computed(() => handleBackground(props.background, true))
 </script>
 
+<style>
+.cover {
+    /* box-shadow: inset 0 0 0 2000px rgba(3, 9, 30, 0.55); */
+    background: rgba(3, 9, 30, 0.8);
+    background-blend-mode: darken;
+}
+</style>
+
 <template>
-    <div class="slidev-layout cover text-center" :style="style"
-    >
-        <img src="/assets/logo.svg" h-15 right-10 fixed c-white>
+    <div class="slidev-layout cover text-center" :style="style">
+        <img src="/logo.svg" h-15 right-10 fixed c-white>
         <div class="my-auto w-full">
             <slot />
+        </div>
+        <div absolute mt-100 w-full>
+            <alert type="info" font-bold>Use arrows to move <typcn-arrow-left-thick /><typcn-arrow-right-thick /></alert>
         </div>
     </div>
 </template>
